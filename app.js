be60ef0fd1f812e6aca6e722cd8dcc3860230c78
Ayodo <37110623+ayoodo@users.noreply.github.com>
2022-03-19 18:03:30 +0100

    update controls

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const allSections = document.querySelector(".main-content");
 const sections = document.querySelectorAll(".section");
 const sectBtns = document.querySelectorAll(".controls");
 const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
 
 function PageTransitions() {
   // Button click active class
@@ -9,11 +9,29 @@ function PageTransitions() {
     sectBtn[i].addEventListener("click", function () {
       let currentBtn = document.querySelectorAll(".active-btn");
       currentBtn[0].className = currentBtn[0].className.replace(
-        " active-btn",
+        "active-btn",
         ""
       );
       this.className += " active-btn";
     });
   }
+  // Sections Active
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      // Remove selected from the other buttons
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+
+      // Hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
 }
 PageTransitions();
